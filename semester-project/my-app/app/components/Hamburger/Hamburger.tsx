d55bb22d8ca62a59d9786673092f0d465f274068
lucijatopic <ltopic01@fesb.hr>
2024-01-16 10:22:04 +0100

    Added small changes to hamburger

--- semester-project/my-app/app/components/Hamburger/Hamburger.tsx
+++ semester-project/my-app/app/components/Hamburger/Hamburger.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { FC, useState } from "react";
 import Image from "next/image";
-import "./Hamburger.css"; // Import your CSS module
+import "./Hamburger.css";
 
 interface HamburgerProps {
   pages: { [key: string]: string };

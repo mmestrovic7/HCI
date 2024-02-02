456a28ec0698b4d50b74de87105815510c2fa40f
[mmestrovic7] <[marijamestrovic47@gmail.com]>
2024-02-02 22:03:06 +0100

    trying to fix the video thumbnail issue on iphone

--- semester-project/my-app/app/components/TikTok/TikTok.tsx
+++ semester-project/my-app/app/components/TikTok/TikTok.tsx
@@ -7,7 +7,7 @@ interface TikTokProps {
 }
 
 const TikTok: React.FC<TikTokProps> = ({ src }) => {
-
+  src=src+"#t=0.001";
   return (
     <video controls className='tikTok' preload='metadata'>
       <source src={src} type="video/mp4" />

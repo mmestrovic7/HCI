6d7b280fab8d9184637c3bde598379ff9650dbad
lucijatopic <ltopic01@fesb.hr>
2024-02-04 18:27:52 +0100

    changes in fans design

--- semester-project/my-app/app/fans/page.tsx
+++ semester-project/my-app/app/fans/page.tsx
@@ -1,18 +1,32 @@
-import Image from "next/image";
-import Link from "next/link";
-import CustomButton from "../components/CustomButton/CustomButton";
-import Footer from "../components/Footer/Footer";
-import "../about-hs/composition.css";
+import Image from 'next/image'
+import Link from 'next/link'
+import CustomButton from '../components/CustomButton/CustomButton';
+import Footer from '../components/Footer/Footer';
+import "../about-hs/composition.css"
+import "./fans.css"
 function Home() {
   return (
     <main>
-      <h1 className="mainTitle">FAN ACTIVITIES</h1>
-      <ul className="subpageElements">
-        <CustomButton text="EXPERIENCES" href="/fans/experiences" />
-        <CustomButton text="PROJECTS" href="/fans/projects" />
-      </ul>
+      <h1 className='mainTitle'>FAN ACTIVITIES</h1>
+        <div className='subpageElements'>
+          <ul className = 'experiences-text'>
+            <p>Read fan stories! Click 'Experiences' to explore Love on Tour through fans' eyes. 🌟
+            Dive into shared experiences and enjoy the journey with us. 🌈</p>
+            <CustomButton text="EXPERIENCES" href="/fans/experiences" />
+          </ul>
+
+          <ul className='projects-text'>
+            <p>Discover cute fan projects! Click 'Projects' to explore the sweetest creations by Love on Tour fans. 
+              Enjoy the charm and creativity in every project. 🌸✨</p>
+            <CustomButton text="PROJECTS" href="/fans/projects" />
+          </ul>
+          
+          </div>
+        
+      
+        
     </main>
-  );
+  )
 }
 
 export default Home;

945a23c2d13c029b1f2be6ac007c1cab38d065ce
lucijatopic <ltopic01@fesb.hr>
2024-02-04 14:19:38 +0100

    minor changes

--- semester-project/my-app/app/fans/page.tsx
+++ semester-project/my-app/app/fans/page.tsx
@@ -1,18 +1,18 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import CustomButton from '../components/CustomButton/CustomButton';
-import Footer from '../components/Footer/Footer';
-import "../about-hs/composition.css"
+import Image from "next/image";
+import Link from "next/link";
+import CustomButton from "../components/CustomButton/CustomButton";
+import Footer from "../components/Footer/Footer";
+import "../about-hs/composition.css";
 function Home() {
   return (
     <main>
-      <h1 >FAN ACTIVITIES</h1>
-        <ul className='subpageElements'>
+      <h1 className="mainTitle">FAN ACTIVITIES</h1>
+      <ul className="subpageElements">
         <CustomButton text="EXPERIENCES" href="/fans/experiences" />
-      <CustomButton text="PROJECTS" href="/fans/projects" />
-        </ul>
+        <CustomButton text="PROJECTS" href="/fans/projects" />
+      </ul>
     </main>
-  )
+  );
 }
 
 export default Home;

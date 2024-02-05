eaf20ecb68074a8770fe848ccd1f444b07d0e8fa
[mmestrovic7] <[marijamestrovic47@gmail.com]>
2024-02-06 00:43:01 +0100

    minor text change

--- semester-project/my-app/app/about-hs/film/page.tsx
+++ semester-project/my-app/app/about-hs/film/page.tsx
@@ -19,7 +19,7 @@ function Home() {
         recognition for his talents in both domains.
       </p>
       <p className="clickToListen">
-        (YOU CAN CLICK ON FILM POSTERS TO CHECK THEIR IMDb pages)
+        (YOU CAN CLICK ON FILM POSTERS TO CHECK THEIR IMDb PAGES)
       </p>
       </div>
       <div className="mainContainer">

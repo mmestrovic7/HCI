4f00d7d8d865f609d4a625b150b9fdc65d145b9a
[mmestrovic7] <[marijamestrovic47@gmail.com]>
2024-02-09 22:18:46 +0100

    changes to fans design

--- semester-project/my-app/app/fans/page.tsx
+++ semester-project/my-app/app/fans/page.tsx
@@ -1,15 +1,17 @@
 import CustomButton from '../components/CustomButton/CustomButton';
 import '../about-hs/abouths.css'
 import "./fans.css"
+import Image from'next/image'
 function Home() {
   return (
-    <main>
+    <main>    
       <div className="fpPage">
-      <h1 className='abouths-title'>FAN ACTIVITIES</h1>
+        <Image src="/couch.png" alt="couch" width={450} height={450} className='couch'/>
+      <h1 className='abouths-title'>WELCOME TO HARRY'S HOUSE!</h1>
         <div className='fpSubpageElements'>
           <ul className = 'experiences-text'>
-            <p>Read fan stories! Click 'Experiences' to explore Love on Tour through fans' eyes. 🌟
-            Dive into shared experiences and enjoy the journey with us. 🌈</p>
+            <p>Read fan stories! Click 'Experiences' to explore Love on Tour through fans' eyes. 
+            Dive into shared experiences and enjoy the journey with us. </p>
 
             <CustomButton text="EXPERIENCES" href="/fans/experiences" />
             
@@ -17,13 +19,13 @@ function Home() {
 
           <ul className='projects-text'>
             <p>Discover cute fan projects! Click 'Projects' to explore the sweetest creations by Love on Tour fans. 
-              Enjoy the charm and creativity in every project. 🌸✨</p>
+              Enjoy the charm and creativity in every project.</p>
             <CustomButton text="PROJECTS" href="/fans/projects" />
           </ul>
           
-          </div> 
-      </div>
-        
+          </div>
+           <Image src="/image 42.png" alt="lamp" width={300} height={100} className='lamp'/>
+      </div> 
     </main>
   )
 }
